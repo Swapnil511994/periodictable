af7ce0b62d6315dd8b920f172562631f1a9b879f
swapnil.vishwakarma <swapnil511994@gmail.com>
2023-05-29 18:27:51 +0530

    Added Period Headers

---
+++ src/Components/PeriodHeaderItem.js
@@ -0,0 +1,4 @@
+export default function PeriodHeaderItem(props)
+{
+    return <div className="period__header__item">{props.text}</div>;
+}
--- src/Components/PeriodicTable.js
+++ src/Components/PeriodicTable.js
@@ -2,6 +2,7 @@ import datum from "../data";
 import React from "react";
 import GroupHeaderItem from "./GroupHeaderItem";
 import GroupPeriodHeaderItem from "./GroupPeriodHeaderItem";
+import PeriodHeaderItem from "./PeriodHeaderItem";
 
 export default function PeriodicTable(props)
 {
@@ -12,18 +13,29 @@ export default function PeriodicTable(props)
 
     let groups = [];
     groups[0] = [];
-    groups[0].push(<GroupPeriodHeaderItem />)
+    groups[0].push(<GroupPeriodHeaderItem />);
+
     for(let i=1;i<=18;i++)
     {
         groups[i] = [];
         groups[i].push(<GroupHeaderItem text={i} />);
     }
-    
 
+    let periodCount = 1;
+    for(let i=1;i<=7;i++)
+    {
+       groups[0].push(<PeriodHeaderItem text={i} />);
+    }
+    
+    let view = [];
+    for(let i=0;i<18;i++)
+    {
+        view.push(<div className="group__container">{groups[i]}</div>);
+    }
 
     return (
         <div className="periodic__div">
-            {groups}
+            {view}
         </div>
     );
 }
