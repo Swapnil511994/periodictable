b9ea40e3b8c45cb18c7911decb7ddca85977663a
swapnil.vishwakarma <swapnil511994@gmail.com>
2023-05-30 15:10:01 +0530

    Style Changes

--- src/Components/PeriodicTableElement.js
+++ src/Components/PeriodicTableElement.js
@@ -1,10 +1,32 @@
 export default function PeriodicTableElement(props)
 {
     let elem = props.element || {};
+    let classVal = "";
+    if(elem.phase)
+    {
+        classVal="periodicElement";
+        switch(elem.phase.toLowerCase())
+        {
+            case "gas": classVal+=" gas";
+            break;
+
+            case "liquid": classVal+=" liquid";
+            break;
+
+            case "solid": classVal+=" solid";
+            break;
+
+            default: classVal+=" unknown__phase";
+            break;
+        }
+    }
+    else classVal="emptyElement";
+
     return (
-        <div className={elem.name?"periodicElement":"emptyElement"}>
+        <div className={classVal}>
             <abbr className="element__number">{elem.number}</abbr>
             <span className="element__symbol">{elem.symbol}</span>
+            <span className="element__name">{elem.name}</span>
         </div>
     );
 }
