9f5474c0e5466bb060bb9019dbba6ce3722d989b
swapnil.vishwakarma <swapnil511994@gmail.com>
2023-06-04 22:12:14 +0530

    Element Information Model Changes, Styling Changes

--- src/Components/PeriodicTableElementInformation.js
+++ src/Components/PeriodicTableElementInformation.js
@@ -36,16 +36,21 @@ export default function PeriodicTableElementInformation(props)
                 <h3>Overview</h3>
                 <table border={0} className="modal__element__table">
                     <tr><td>Atomic Mass</td><td>{element.atomic_mass}</td></tr>
-                    <tr><td>Category</td><td>{Capify(element.category)}</td></tr>
                     <tr><td>Atomic Number</td><td>{element.number}</td></tr>
                     <tr><td>Group</td><td>{element.group}</td></tr>
                     <tr><td>Period</td><td>{element.atomic_mass}</td></tr>
+                    {element.named_by && <tr><td>Named By</td><td>{Capify(element.named_by)}</td></tr>}
+                    {element.discovered_by && <tr><td>Discovered By</td><td>{Capify(element.discovered_by)}</td></tr>}
                     <tr><td>Phase</td><td>{Capify(element.phase)}</td></tr>
+                    <tr><td>Category</td><td>{Capify(element.category)}</td></tr>
                     <tr><td>Electronic Configuration</td><td>{element.electron_configuration}</td></tr>
-                    {/* <tr><td>EC Semantic</td><td>{element.electron_configuration_semantic}</td></tr> */}
-                    <tr><td>Atomic Mass</td><td>{element.atomic_mass}</td></tr>
+                    {element.appearance && <tr><td>Appearence</td><td>{Capify(element.appearance)}</td></tr>}
+                    {element.melt && <tr><td>Melt</td><td>{element.melt + " K"}</td></tr>}
+                    {element.boil && <tr><td>Boil</td><td>{element.boil + " K"}</td></tr>}
+                    <tr><td>Source</td><td><a href={element.source} className="element__source" target="_blank" >Wikipedia ↗</a></td></tr>
                 </table>
             </div>
+            
             <div className="modal__3d">
                 <model-viewer
                     alt={element.name+"'s 3D model"}
@@ -55,7 +60,7 @@ export default function PeriodicTableElementInformation(props)
                     shadow-intensity="0" camera-controls touch-action="pan-y"
                     autoplay
                 >
-            </model-viewer>
+                </model-viewer>
             </div>
         </div>
     );
